93a117d601101aa838d64b864c77b475b2eac35c
SCollins <sje.collins@gmail.com>
2023-03-02 15:57:09 +0000

    Sidebar style

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import css from './css/Sidebar.module.css'
 
 const Sidebar = () => {
-    return <div>
-        <a href="">My Photos</a>
-        <a href="">My Illustrations</a>
-        <a href="">My Paintings</a>
+    return <div className={css.sidebar}>
+        <a href="#.">My Photos</a>
+        <a href="#.">My Illustrations</a>
+        <a href="#.">My Paintings</a>
     </div>
 }
 
